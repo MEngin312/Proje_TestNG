579bc85542ddb58adbf4d58d196138987e1b26d2
kadir <kadir.tepecik1903@gmail.com>
2022-10-29 19:37:08 +0300

    29/10/2022

--- com.HepsiBurada.TestNG/src/test/java/tests/HepsiBuradaTest.java
+++ com.HepsiBurada.TestNG/src/test/java/tests/HepsiBuradaTest.java
@@ -15,16 +15,22 @@ public class HepsiBuradaTest extends TestBaseRapor {
     public void kullaniciOlarakTest(){
          hb=new HepsiBuradaPage();
          Log.startTestCase("Kullanici Olarak Test");
+
          hb.istenilenSayfayaGitme();
          Log.info("Kullanici "+ConfigReader.getProperty("hepsiUrl")+" sayfasina gider.");
+
          hb.girisYapma();
          Log.assertLog("Kullanici olarak giris yapma ve dogrulama.");
+
          hb.aramaYapipUrunSecme();
          Log.info("Kullanici "+ConfigReader.getProperty("arananKelime")+" kelimesini aratır ve urune tıklar.");
+
          hb.secilenUrunuSepeteEklemevefarkliSaticiEkleme();
          Log.info("Kullanici sectigi urunu sepete ekler ve varsa farklı satıcıdan ikinci urunu ekler.");
+
          hb.sepetimGitveSecilenUrunleriDogrulama();
          Log.assertLog("Kullanici sepete gider ve bilgileri dogrular.");
+
          hb.secilenUrunleriSilme();
          Log.info("Kullanici sectigi urunleri sepetten siler.");
          Log.endTestCase("Kullanici Olarak Test");
